9a7014f683611cf32388c8870c9b3cd9d55b0217
Sanketh Kumar <sankethkumarbg1995@gmail.com>
2022-12-28 13:32:58 +0530

    edit

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-    define("num",500);
-    echo num;
+    echo "Hello World<br>";
+    $sanketh = "Its all well<br>";
+    define("keerthi","Its better than urs<br>");
+    echo $sanketh;
+    echo keerthi;
+    // echo "<br>";
+    // echo var_dump($sanketh); 
 
 ?>
